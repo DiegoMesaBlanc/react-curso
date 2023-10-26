2d73d9b1d90276683eebedc5865de8fdfdd77790
Diego Mesa <dhiego__07@hotmail.com>
2023-10-26 15:30:33 -0500

    Anecdotes done

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,12 +1,57 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
-  const [selected, setSelected] = useState(1)
+
+const Display = ({ text }) => {
+  return (
+    <div>
+      <h2>{text}</h2>
+    </div>
+  )
+}
+
+const Text = ({ text, voteText }) => {
+  return (
+    <div>
+      <p>{text}</p>
+      <p>{voteText}</p>
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0)
+  const [maximo, setMax] = useState(null)
+  const [points, setPoints] = useState({})
+
+  const randomIntFromInterval = (min, max) => () => {
+    const number = Math.ceil(Math.random() * max) + min
+    setSelected(number)
+  }
+
+  const addRate = () => {
+    const pts = { ...points }
+    pts[selected] = (pts[selected] || 0) + 1
+    setPoints(pts)
+    const maxx = Object.keys(pts).reduce((a, b) => pts[a] > pts[b] ? a : b);
+    setMax(maxx)
+  }
 
   return (
     <div>
-      {props.anecdotes[selected]}
+      <Display text="Anecdote of the day" />
+      <Text text={anecdotes[selected]} voteText={`hasta ${points[selected] ?? 0} votes`} />
+      <Button handleClick={addRate} text='vote' />
+      <Button handleClick={randomIntFromInterval(0, 5)} text='next anecdote' />
+
+      <Display text="Anecdote with most votes" />
+      <Text text={anecdotes[maximo]} voteText={points[maximo] ? 'hasta ' + points[maximo] + ' votes' : null} />
     </div>
   )
 }
